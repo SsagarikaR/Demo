bcc480c546bc260b45337a7dd7ab89ce2691820e
SsagarikaR <Sagarikar@mindfiresolutions.com>
2025-01-21 11:00:03 +0530

    created html folder for typescript form

--- modules/cityObj.js
+++
@@ -1,7 +0,0 @@
-export const city = {
-    AndhraPradesh: ["Vijaywada", "Tirupati", "Anantapur"],
-    ArunachalPradesh: ["Namsal", "Roing", "Ziro"],
-    Assam: ["Guwahati", "Dibrugarh", "Dhubri"],
-    Bihar: ["Patna", "Gaya", "Bhagalpur"],
-    Odisha: ["Bhubaneswar", "Cuttack", "Puri"],
-  };
